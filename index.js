c7a6ebe2d60e63abffd3d9db22f24a32e52e784e
6174 <570171025@qq.com>
2016-03-25 15:13:31 +0800

    去除外部字体依赖

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ module.exports = {
         app.import(bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
         app.import(bowerDirectory + '/bootstrap-validator/js/validator.js');
         app.import(bowerDirectory + '/bootstrap-sweetalert/lib/sweet-alert.js');
-        app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
-        app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
-        app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
-        app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
-        app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
-        app.import(bowerDirectory + "/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts"});
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+        // app.import(bowerDirectory + "/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts"});
+        ['eot', 'svg', 'ttf', 'woff'].forEach(function(type) {
+            app.import('vendor/styles/antd/font/iconfont.' + type, {destDir: "fonts/antd"});
+            // app.import('vendor/lib/font-awesome/fonts/fontawesome-webfont.' + type, {destDir: "fonts"});
+        });
     }
 };
