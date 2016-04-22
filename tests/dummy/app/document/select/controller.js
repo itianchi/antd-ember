c8d22b0f84083580b578bb693c2b8b87320e17fc
6174 <570171025@qq.com>
2016-04-22 15:10:47 +0800

    树选择组件

--- tests/dummy/app/document/select/controller.js
+++ tests/dummy/app/document/select/controller.js
@@ -1,9 +1,36 @@
 import Ember from 'ember';
 
+const treeData = [{
+  label: '节点一',
+  value: '0-0',
+  children: [{
+    label: '子节点一',
+    value: '0-0-0',
+  }, {
+    label: '子节点二',
+    value: '0-0-1',
+  }],
+}, {
+  label: '节点二',
+  value: '0-1',
+  children: [{
+    label: '子节点三',
+    value: '0-1-0',
+  }, {
+    label: '子节点四',
+    value: '0-1-1',
+    children: [{
+    	label: '子节点五',
+    	value: '0-1-1-1'
+    }]
+  }],
+}];
 export default Ember.Controller.extend({
 	checked: '1',
 	value: '',
 	values: ['lucy', 'jack'],
+	treeData: treeData,
+	treeSelectedValue: '0-1-0',
 	options: [{
 		value: '1',
 		label: 'jack'
