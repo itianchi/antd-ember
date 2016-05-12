9b9061b8292fc565e22e01b29d556b063d77a68f
6174 <570171025@qq.com>
2016-05-12 16:52:02 +0800

    tree-select demo 修改

--- tests/dummy/app/document/select/controller.js
+++ tests/dummy/app/document/select/controller.js
@@ -2,26 +2,26 @@ import Ember from 'ember';
 
 const treeData = [{
   title: '节点一',
-  value: '0-0',
+  id: '0-0',
   children: [{
     title: '子节点一',
-    value: '0-0-0',
+    id: '0-0-0',
   }, {
     title: '子节点二',
-    value: '0-0-1',
+    id: '0-0-1',
   }],
 }, {
   title: '节点二',
-  value: '0-1',
+  id: '0-1',
   children: [{
     title: '子节点三',
-    value: '0-1-0',
+    id: '0-1-0',
   }, {
     title: '子节点四',
-    value: '0-1-1',
+    id: '0-1-1',
     children: [{
     	title: '子节点五',
-    	value: '0-1-1-1'
+    	id: '0-1-1-1'
     }]
   }],
 }];
